28bab5566e46d6b2c1058b99da49f85d5db9f8db
Livia Kamrad <Kwadrat777@gmail.com>
2024-04-04 15:55:25 +0200

    save

---
+++ AufgabeFunktionen/Scripts/Script.js
@@ -0,0 +1,30 @@
+﻿
+function probability(a)
+{
+
+    let arr = [0, 0, 0, 0, 0, 0]
+
+    for (let i = 0; i < a; i++)
+    {
+        let wurfel = Math.random() * 6;
+        let z = Math.floor(wurfel);
+        arr[z]++;
+    }
+    return arr;
+}
+function ausgabe(wurfe, ...numbers)
+{
+    console.log("Würfelstatistik");
+    console.log(`Bei ${wurfe} Würfen entfielen`);
+    for (let i in numbers)
+    {
+        console.log("auf die 1: "+ i +" Würfe");
+    }
+}
+
+let wurfe = prompt("Würzelwürfe", "");
+
+
+let result = probability(wurfe);
+
+ausgabe(wurfe, result);
